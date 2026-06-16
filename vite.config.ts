112415e5c34c8713f71420bb1fb1fcf0dc84cbe7
Imago_Fotodiseño <imagofotodiseno@gmail.com>
2026-06-16 13:01:07 -0500

    vr2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/sitio-wen-hotel-carolina-del-mar/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
